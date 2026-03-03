f0c90e142256a357bab47ba5c91824e44d072d6f
rawr1de <rdo6@msn.com>
2026-03-03 07:49:57 -0500

    fzf search improved, deleted old entries, zsh modified

--- common/scripts/.scripts/editor_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script Editor (search files in .scripts/ .config/ Docs/linux_shit/
-
-du -a . | awk '{print $2}' | fzf -i | xargs -I{} emacsclient -c {}
-
-# complementar scrip com condicional SE;
-# se for .pdf ou qualquer outro arq. executável pelo zathura abrir nele
-# caso n seja, abrir no emacs
