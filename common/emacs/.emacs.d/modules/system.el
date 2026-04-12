6413420f70ec2075bc34d8c79fba6a0df33855e3
rawr1de <83790233+rawr1de@users.noreply.github.com>
2026-04-12 11:24:47 -0400

    system/tools .org updates..

--- common/emacs/.emacs.d/modules/system.el
+++ common/emacs/.emacs.d/modules/system.el
@@ -1,35 +1,29 @@
-;; -*- lexical-binding: t; -*-
-;;
-;;; --- PERFORMANCE ENGINE
-  (setq gc-cons-threshold 100000000)
-  (add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+(setq gc-cons-threshold 100000000)
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
 
+(setq use-short-answers t)
+(setq inhibit-startup-message t
+      ring-bell-function 'ignore
+      scroll-preserve-screen-position t
+      scroll-step 1
+      create-lockfiles nil
+      case-fold-search t
+      read-quoted-char-radix 10)
+;; controls number system (base) numerical code
+;; Default (8): Octal (0-7) type C-q 141, get "a"
+;; Value (10): Decimal type C-q 97 get "a"
+;; Value (16): Hexadecimal type C-q 61 get "a"
+;; enable recent files tracking
+      (recentf-mode 1)
+;; maximum number of saved items
+      (setq recentf-max-saved-items 50)
 
-;;; --- SYSTEM IMPROVEMENTS
-  (setq use-short-answers t)
-  (setq inhibit-startup-message t
-        ring-bell-function 'ignore
-        scroll-conservatively 100
-        scroll-preserve-screen-position t
-        create-lockfiles nil
-        case-fold-search t
-        read-quoted-char-radix 10)
-  ;; controls number system (base) numerical code
-  ;; Default (8): Octal (0-7) type C-q 141, get "a"
-  ;; Value (10): Decimal type C-q 97 get "a"
-  ;; Value (16): Hexadecimal type C-q 61 get "a"
-  ;; enable recent files tracking
-        (recentf-mode 1)
-  ;; maximum number of saved items
-        (setq recentf-max-saved-items 50)
-
-  ;; FORCE kill-buffer to quit without saving
-  (defun my-kill-buffer-no-confirm ()
-    "Kill buffer without save prompt"
-    (interactive)
-    (set-buffer-modified-p nil)
-    (kill-buffer))
-
+;; FORCE kill-buffer to quit without saving
+(defun my-kill-buffer-no-confirm ()
+  "Kill buffer without save prompt"
+  (interactive)
+  (set-buffer-modified-p nil)
+  (kill-buffer))
 
 (use-package ibuffer
   :ensure nil
@@ -57,84 +51,60 @@
 
   ;; kill all "non-file" buffers & unused in a while
 
+(setq make-backup-files t)
+(setq auto-save-default t)
+;; Create quarantine folders for backups and auto-saves
+(make-directory "~/.emacs.d/backups/" t)
+(make-directory "~/.emacs.d/auto-saves/" t)
 
-;;; --- KEEP BACKUP/AUTO-SAVE FILES
-  (setq make-backup-files t)
-  (setq auto-save-default t)
-  ;; Create quarantine folders for backups and auto-saves
-  (make-directory "~/.emacs.d/backups/" t)
-  (make-directory "~/.emacs.d/auto-saves/" t)
+;; Route all backup (~) and auto-save (#) files to the quarantine
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups/")))
+(setq auto-save-file-name-transforms
+      '((".*" "~/.emacs.d/auto-saves/" t)))
 
-  ;; Route all backup (~) and auto-save (#) files to the quarantine
-  (setq backup-directory-alist '(("." . "~/.emacs.d/backups/")))
-  (setq auto-save-file-name-transforms
-        '((".*" "~/.emacs.d/auto-saves/" t)))
-
-
-  ;;; --- SYSTEM LANGUAGE/CODING ENVIRONMENT
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 
-  ;;; --- XDG OPEN
-    (setq browse-url-browser-function 'browse-url-firefox)
+;; set Firefox as default web browser
+(setq browse-url-browser-function 'browse-url-firefox)
 
+(add-to-list 'auto-mode-alist '("\\.m3u\\'"  . text-mode))
+(add-to-list 'auto-mode-alist '("\\.m3u8\\'" . text-mode))
+(add-to-list 'auto-mode-alist '("\\.pls\\'"  . text-mode))
+(add-to-list 'auto-mode-alist '("\\.log\\'"  . text-mode))
 
-;;; --- KNOWN FILE TYPES
-    (add-to-list 'auto-mode-alist '("\\.m3u\\'"  . text-mode))
-    (add-to-list 'auto-mode-alist '("\\.m3u8\\'" . text-mode))
-    (add-to-list 'auto-mode-alist '("\\.pls\\'"  . text-mode))
-    (add-to-list 'auto-mode-alist '("\\.log\\'"  . text-mode))
+(use-package savehist
+  :init
+  (setq savehist-file "~/.emacs.d/history"
+        history-length 1000
+        history-delete-duplicates t)
+  (savehist-mode 1)
+  :config
+  (add-to-list 'savehist-additional-variables 'query-replace-history))
 
+(defun config-visit ()
+  (interactive)
+  (find-file "~/.emacs.d/base_cfg.org"))
+(global-set-key (kbd "C-c 9") 'config-visit)
 
-;;; --- SAVE HISTORY
-  (use-package savehist
-    :init
-    (setq savehist-file "~/.emacs.d/history"
-          history-length 1000
-          history-delete-duplicates t)
-    (savehist-mode 1)
-    :config
-    (add-to-list 'savehist-additional-variables 'query-replace-history))
+(defun config-reload ()
+  (interactive)
+  (org-babel-load-file (expand-file-name "~/.emacs.d/base_cfg.el")))
+(global-set-key (kbd "C-c 0") 'config-reload)
 
+;; real-time auto-refresh on dired/dirvish
+;; Auto-refresh Dired/Dirvish buffers when files change on disk
+(add-hook 'dired-mode-hook 'auto-revert-mode)
 
-;;; --- OPEN/RELOAD CONFIG FILE [C-c 9 / C-c 0]
-  (defun config-visit ()
-    (interactive)
-    (find-file "~/.emacs.d/base_cfg_1.org"))
-  (global-set-key (kbd "C-c 9") 'config-visit)
+;; Also, make sure Emacs uses 'ls-lisp' or 'gls' for better update handling
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil) ; Stay quiet while refreshing
 
-  (defun config-reload ()
-    (interactive)
-    (org-babel-load-file (expand-file-name "~/.emacs.d/base_cfg_1.el")))
-  (global-set-key (kbd "C-c 0") 'config-reload)
-
-  ;; real-time auto-refresh on dired/dirvish
-  ;; Auto-refresh Dired/Dirvish buffers when files change on disk
-  (add-hook 'dired-mode-hook 'auto-revert-mode)
-
-  ;; Also, make sure Emacs uses 'ls-lisp' or 'gls' for better update handling
-  (setq global-auto-revert-non-file-buffers t)
-  (setq auto-revert-verbose nil) ; Stay quiet while refreshing
-
-  ;; auto-refreshes screen after a dired/dirvish action
-  (advice-add 'dired-do-copy :after (lambda (&rest _) (revert-buffer)))
-  (advice-add 'dired-do-rename :after (lambda (&rest _) (revert-buffer)))
-  (advice-add 'dired-do-delete :after (lambda (&rest _) (revert-buffer)))
-
-
+;; auto-refreshes screen after a dired/dirvish action
+(advice-add 'dired-do-copy :after (lambda (&rest _) (revert-buffer)))
+(advice-add 'dired-do-rename :after (lambda (&rest _) (revert-buffer)))
+(advice-add 'dired-do-delete :after (lambda (&rest _) (revert-buffer)))
 
 (require 'my-clean-unused-buffers)
 (require 'my-xfk-universal-snap)
 (provide 'system)
-
-
-;;; --- END OF FILE !!!
-;;
-;; M-x package-delete (delete installed files)
-;; M-x package-autoremove (remove package dependencies)
-;;
-;; Local Variables:
-;; eval: (outline-minor-mode 1)
-;; eval: (local-set-key (kbd "<tab>") 'outline-cycle)
-;; outline-regexp: ";;;+ ?---"
-;; End:
